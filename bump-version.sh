2510ce1d7a421b3893ac143bce038d72fa02846f
Bogdan <bogdannbv@gmail.com>
2023-04-13 19:42:24 +0300

    Refactor bump-version.sh to echo a commit message

--- bump-version.sh
+++ bump-version.sh
@@ -1,22 +1,26 @@
 #!/usr/bin/env bash
 
 VERSIONS_FILE=versions.json
-VERSION=$1
+LATEST_VERSION=$1
+
+CURRENT_VERSION=$(jq -r '.latest' "${VERSIONS_FILE}")
 
 function ver {
   printf "%03d%03d%03d" $(echo "$1" | tr '.' ' ');
 }
 
-if [ -z "$VERSION" ]; then
+if [ -z "${LATEST_VERSION}" ]; then
   echo "Usage: $0 <version>"
   exit 1
 fi
 
-if [ "$(ver "${VERSION}")" -le "$(ver "$(jq -r '.latest' "${VERSIONS_FILE}")")" ]; then
-  echo "Version must be greater than $(jq -r '.latest' "${VERSIONS_FILE}")"
-  exit 1
+if [ "$(ver "${LATEST_VERSION}")" -le "$(ver "${CURRENT_VERSION}")" ]; then
+  echo "Version must be greater than ${CURRENT_VERSION}"
+  exit 2
 fi
 
-VERSIONS=$(jq --arg version "${VERSION}" '.latest |= $version' "${VERSIONS_FILE}")
+LATEST_VERSIONS=$(jq --arg version "${LATEST_VERSION}" '.latest |= $version' "${VERSIONS_FILE}")
 
-echo "${VERSIONS}" > "${VERSIONS_FILE}"
+echo "${LATEST_VERSIONS}" > "${VERSIONS_FILE}"
+
+echo "Update latest version from ${CURRENT_VERSION} to ${LATEST_VERSION}"
